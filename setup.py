79182620f70cb9c480e8bb439c8e5fc6e333c4bd
Arun Babu Neelicattu <abn@redhat.com>
2013-02-26 19:23:32 +1000

    Initializing setup files and source directory.

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+setup(
+	name='pyrus',
+	version='0.0.0',
+	description='Python helper module.',
+	long_description='',
+	author='Arun Babu Neelicattu',
+	url="https://github.com/abn/pyrus",
+	download_url="https://github.com/abn/pyrus",
+
+	install_requires=[],
+
+	# license="",
+
+	packages=find_packages('src'),
+	package_dir={'': 'src'},
+	include_package_data=True,
+
+	# test_suite="",
+
+	classifiers=[
+		'Intended Audience :: Developers',
+		'Programming Language :: Python',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+	],
+)
