68dde3096fcfbd5e9992e88f614aaf6927b6519c
Arun Babu Neelicattu <abn@redhat.com>
2013-09-14 23:45:11 +1000

    Iniital rework

--- src/pyrus/__init__.py
+++ src/pyrus/__init__.py
@@ -1,134 +1,35 @@
 from abc import abstractmethod, ABCMeta
-from multiprocessing import Manager, Process
+from multiprocessing import Manager
 from multiprocessing.util import ForkAwareThreadLock
-from multiprocessing.queues import Empty
-from os import urandom
-from time import sleep
+
 
 def enum(**enums):
-	return type('Enum', (), enums)
+    return type('Enum', (), enums)
 
-class AbstractMPBorg(metaclass=ABCMeta):
-	_mutex = ForkAwareThreadLock()
-	def __init__(self, *args, **kwds):
-		self._mutex.acquire()
-		try:
-			if not hasattr(type(self), '_shared_state'):
-				type(self)._shared_state = {}
-			self.__dict__ = self._shared_state
-			if not self.is_initialized():
-				self._manager = Manager()
-				self._initialize(*args, **kwds)
-				self.initialized = self._manager.Value(bool, True)
-		finally:
-			self._mutex.release()
 
-	def is_initialized(self):
-		"""Tests if the shared state was initialized."""
-		return self.__dict__.get('initialized', False)
+class MultiProcessBorg():
+    __metaclass__ = ABCMeta
+    _mutex = ForkAwareThreadLock()
 
-	@abstractmethod
-	def _initialize(self, *args, **kwds):
-		pass
+    def __init__(self, *args, **kwds):
+        self._mutex.acquire()
+        try:
+            if not hasattr(type(self), '_shared_state'):
+                type(self)._shared_state = {}
+            self.__dict__ = self._shared_state
+            if not self.is_initialized():
+                self._manager = Manager()
+                self._initialize(*args, **kwds)
+                self.initialized = self._manager.Value(bool, True)
+        finally:
+            self._mutex.release()
 
-SHUTDOWN_WAIT_TIMEOUT = 5
-QUEUE_GRACE_PERIOD = 0.1
+    def is_initialized(self):
+        """
+        Is the shared state initialized?
+        """
+        return self.__dict__.get('initialized', False)
 
-class AbstractQueueConsumer(AbstractMPBorg):
-	def __init__(self, consumers, *args, **kwds):
-		AbstractMPBorg.__init__(self, consumers, *args, **kwds)
-
-	@property
-	def queue(self):
-		return self._queue
-
-	def _initialize(self, consumers):
-		"""Internal method to initialize all global variables. This initializes
-		the manager, queue, pool and trigger the consumer.
-
-		If additional initializations need to be done, implentor can overwrite
-		this method. Note that this should be called after additional
-		initializations are performed."""
-		self._terminator = 'TERMINATE'.encode() + urandom(10)
-		self._queue = self._manager.Queue(-1)
-		self._consumers = consumers
-		self._start_consumers(self._consumers)
-
-	def shutdown(self, timeout=SHUTDOWN_WAIT_TIMEOUT):
-		"""Kick starts the shut-down process for the class"""
-		self._stop_consumers(timeout)
-		if self._process.is_alive():
-			# We kill the process if it did not agree to die
-			self._process.terminate()
-			if not self.queue.empty():
-				msg = 'Killed log consumer with messages still in queue.'
-				print(type(self), msg)
-
-	def blocking_flush(self):
-		"""Process all contents as if shutting down, and restart consumers"""
-		self.shutdown(None)
-		self._start_consumers(self._consumers)
-
-	@abstractmethod
-	def _record_handler(self, *args):
-		"""Performs the desired action on the record received."""
-		pass
-
-	def _put(self, *args):
-		record = tuple(args)
-		self.queue.put(record)
-
-	def _start_consumers(self, consumers):
-		"""Starts the requested number of consumers
-
-		The last consumer is marked as the 'sucker', this is the only consumer
-		that will wait for queue to be empty after the terminate message is
-		received.
-		"""
-		for i in range(consumers - 1):
-			p = Process(target=self._consumer, args=(i,))
-			p.start()
-		self._process = Process(target=self._consumer,
-							args=(consumers - 1, True))
-		self._process.start()
-
-	def _stop_consumers(self, timeout):
-		"""Stops all consumers by sending as many terminate messages as there
-		are consumers.
-
-		This method waits till the original process that initiated the consumers
-		finish."""
-		for _ in range(self._consumers):
-			self.queue.put(self._terminator)
-		self._process.join(timeout)
-
-	def _consumer(self, cid, sucker=False):
-		"""Starts consuming from this instance's queue.
-
-		This functions does nothing	if called once the instance's 'initialized'
-		flag is already set.
-
-		The will block till a new record is received from the queue. If the new
-		record is this instance's terminate key, the function breaks when
-		sucker=False. If sucker is True, the function will wait till all records
-		are consumed."""
-		if self.is_initialized():
-			return None
-		terminate = False
-		while not ((terminate and sucker and self.queue.empty()) \
-				or (terminate and not sucker)):
-			self._mutex.acquire()
-			try:
-				record = self.queue.get(True)
-			except Empty as _:
-				# We should not get this, but just in case.
-				pass
-			finally:
-				self._mutex.release()
-			if isinstance(record, bytes) and self._terminator == record:
-				terminate = True
-				# Allow a grace period
-				if sucker:
-					sleep(QUEUE_GRACE_PERIOD)
-			else:
-				self._record_handler(*record)
+    @abstractmethod
+    def _initialize(self, *args, **kwds):
+        pass
--- src/pyrus/mplogging.py
+++ src/pyrus/mplogging.py
@@ -1,172 +1,108 @@
-import atexit
-import time
-from logging import NOTSET, INFO, DEBUG, WARN, ERROR, CRITICAL
-from logging import addLevelName, getLevelName
-from multiprocessing import Process, current_process
-from multiprocessing.managers import BaseManager, BaseProxy
-from pyrus import AbstractQueueConsumer
+from atexit import register
+from multiprocessing import Queue
+from logging import getLogger, Formatter, StreamHandler
+from logging.handlers import QueueHandler, QueueListener
 
-DFAULT_LOG_LEVEL = INFO
+from pyrus import MultiProcessBorg
 
-class LogMessage():
-	def __init__(self, name, level, msg, pid=None):
-		self.logtime = time.localtime()
-		self.level = level
-		self.msg = msg
-		self.name = name
-		if pid:
-			self.pid = pid
-		else:
-			self.pid = current_process().pid
 
-	def __str__(self):
-		level = getLevelName(self.level)
-		return '[%s] [%s] [%s] [%s] %s' % (time.asctime(self.logtime), level,
-						self.name, self.pid, self.msg)
+DEFAULT_FORMAT = '[%(asctime)s] [%(process)d] [%(levelname)s] [%(name)s] ' +\
+    '%(message)s'
+ROOTLOGGER = 'pyrus'
 
-class _Logging(AbstractQueueConsumer):
-	"""_Logging class is a multiprocessing safe logging class. This class is
-	designed with the Borg DP in mind . (All instances of this class shares the
-	same states.) An instance of this class is to be used to get loggers.
 
-	By design, this acts like a server consuming messages from a MP Queue. The
-	loggers received from get_logger() methed can communicate using this Queue.
-	"""
-	def __init__(self, consumers=1):
-		AbstractQueueConsumer.__init__(self, consumers)
+class MultiprocessLogManager(MultiProcessBorg):
+    """
+    A multiprocessing friendly log manager with shared state across processes.
+    """
+    def __init__(self):
+        super(MultiprocessLogManager, self).__init__()
 
-	@property
-	def level(self):
-		try:
-			return self.__level
-		except:
-			return NOTSET
+    def _initialize(self):
+        self.loggers = {}
+        self.queue = Queue(-1)
+        self.queue_handler = QueueHandler(self.queue)
+        self.handler = StreamHandler()
+        self.listener = QueueListener(self.queue, self.handler)
+        self.listener.start()
+        self.logformat = DEFAULT_FORMAT
 
-	@level.setter
-	def level(self, level):
-		self.__level = level
+    @property
+    def logformat(self):
+        return self.handler.formatter._fmt
 
-	def get_log_level(self):
-		return self.level
+    @logformat.setter
+    def logformat(self, fmt):
+        self.set_formatter(Formatter(fmt))
 
-	def set_log_level(self, level):
-		self.level = level
+    @property
+    def loglevel(self):
+        return self.handler.level
 
-	def addLevelName(self, level, levelName):
-		"""Method adds a new level with a given name. This is just a wrapper for
-		logging.addLevelName."""
-		addLevelName(level, levelName)
+    @loglevel.setter
+    def loglevel(self, level):
+        self.handler.setLevel(level)
 
-	def _initialize(self, consumers):
-		"""Internal method to initialize all global variables. This initializes
-		the manager, queue, pool and trigger the consumer."""
-		self.level = DFAULT_LOG_LEVEL
-		# This is used to securely terminate the logging process once started
-		AbstractQueueConsumer._initialize(self, consumers)
+        for logger in self.loggers.values():
+            logger.setLevel(level)
 
-	def __log_direct(self, name, level, message):
-		log = str(LogMessage(name, level, message))
-		print(log)
+    def set_formatter(self, formatter):
+        """
+        Set the formatter instance for the manager
 
-	def _record_handler(self, record):
-		print(record)
+        :Parameters:
+            `formatter`: The `logging.Formatter` instance to use
+        """
+        self.handler.setFormatter(formatter)
 
-	def log(self, name, level, msg, pid):
-		log = str(LogMessage(name, level, msg, pid))
-		self._put(log)
+    def get_logger(self, name=ROOTLOGGER):
+        """
+        Get a named logger. A new logger is created and added to the shared
+        state if none exists, else the existing logger is returned.
 
-class _Logger():
-	"""A poor man's implementation of a _Logger class for the use in
-	_Logging.get_logger()."""
-	def __init__(self, name, level, server, pid):
-		self.level = level
-		self.name = name
-		self.server = server
+        :Parameters:
+            `name`: Logger's name. Defaults to `ROOTLOGGER`.
+        """
+        if name in self.loggers:
+            return self.loggers[name]
 
-	def _log(self, pid, level, msg):
-		if level >= self.level:
-			args = (self.name, level, msg, pid)
-			p = Process(target=self.server.log, args=args)
-			p.start()
+        logger = getLogger(name)
+        logger.addHandler(self.queue_handler)
 
-	def get_log_level(self):
-		return self.level
+        self.loggers[name] = logger
+        return logger
 
-	def set_log_level(self, level):
-		self.level = level
+    def stop(self):
+        """
+        Stops this instance of the manager
+        """
+        self.listener.stop()
 
-	def error(self, pid, msg):
-		self._log(pid, ERROR, msg)
 
-	def critical(self, pid, msg):
-		self._log(pid, CRITICAL, msg)
+manager = MultiprocessLogManager()
 
-	def info(self, pid, msg):
-		self._log(pid, INFO, msg)
 
-	def warning(self, pid, msg):
-		self._log(pid, WARN, msg)
+def get_logger(name=ROOTLOGGER):
+    """
+    Get a named logger
+    """
+    return manager.get_logger(name)
 
-	def warn(self, pid, msg):
-		self._log(pid, WARN, msg)
 
-	def debug(self, pid, msg):
-		self._log(pid, DEBUG, msg)
+def set_format(fmt):
+    """
+    Set the global log format
+    """
+    manager.logformat = fmt
 
-	def log(self, pid, level, msg):
-		self._log(pid, level, msg)
 
-class _LoggerProxy(BaseProxy):
-	def __init__(self, token, serializer, manager=None,
-		authkey=None, exposed=None, incref=True):
-		BaseProxy.__init__(self, token, serializer, manager=manager, authkey=authkey, exposed=exposed, incref=incref)
-		self.pid = current_process().pid
+def setLevel(level):
+    """
+    Set the global loging level
+    """
+    manager.loglevel = level
 
-	# Generate normal proxy methods
-	for meth in ['get_log_level', 'set_log_level']:
-		exec('''def %s(self, *args, **kwds):
-		return self._callmethod(%r, args, kwds)''' % (meth, meth))
 
-	# Generate proxy methods that require current pid
-	for meth in ['critical', 'log', 'info', 'warning', 'warn', 'debug']:
-		exec('''def %s(self, *args, **kwds):
-		pid = current_process().pid
-		return self._callmethod(%r, (pid,) + args, kwds)''' % (meth, meth))
-
-# A simple manager so we are multiprocessing safe
-class LoggingManager(BaseManager): pass
-
-# Register classes
-LoggingManager.register('Logging', _Logging)
-LoggingManager.register('Logger', _Logger, _LoggerProxy)
-
-# Start the module logging manager
-__logging_manager = LoggingManager()
-__logging_manager.start()
-
-# The global instance of _Logging to kickstart log consumer on import
-__mplogging = __logging_manager.Logging()
-
-def Logger(name=__name__, level=None):
-	"""Returns a _Logger instance for the given name. If no level is given
-	the global level is used. The class state caches the loggeres that are
-	created and reuses them as required."""
-	if level is None:
-		level = __mplogging.get_log_level()
-	pid = current_process().pid
-	return __logging_manager.Logger(name, level, __mplogging, pid)
-
-def set_log_level(level):
-	"""Sets the global logging level"""
-	__mplogging.set_log_level(level)
-
-def get_log_level():
-	"""Returns the global logging level"""
-	return __mplogging.get_log_level()
-
-@atexit.register
-def __graceful_shutdown():
-	"""This method triggers the shutdown of the logging consumer. This is
-	triggerred only when the python interpreter exits."""
-	Logger(__name__).debug('Shutting down logging')
-	__mplogging.shutdown()
+@register
+def stop():
+    manager.stop()
